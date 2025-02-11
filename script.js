ea424b46f1d615cc09b223402fc0eadcb57337c1
Abdul Kader <abdulkaderovi93@gmail.com>
2025-02-11 21:56:38 +0600

    second commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function addttion(a, b){
+    const result = a+b;
+    return result;
+}
+
+const calculatedResult=addttion(5,7);
+
+console.log(calculatedResult);
+
+
+function juicer(apple, orange){
+    const recipe=`Make juice with ${apple} apples and ${orange} oranges`;
+    return recipe;
+}
+
+const fruitJuice=juicer(3,5);
+console.log(fruitJuice);
