2efd2061dd220d5369ed2260a972cb81941f7e74
Abdul Kader <abdulkaderovi93@gmail.com>
2025-02-16 18:13:03 +0600

    a

--- script.js
+++ script.js
@@ -109,41 +109,41 @@
 
 
 //-------------Array--------------------------------
-const friends=['Sagor','Safiq','Bristy'];
-console.log(friends);
-console.log(friends[friends.length-1]);
-// add element in array
-const newLength=friends.push('Saiful');
-console.log(friends);
-console.log(newLength);
+// const friends=['Sagor','Safiq','Bristy'];
+// console.log(friends);
+// console.log(friends[friends.length-1]);
+// // add element in array
+// const newLength=friends.push('Saiful');
+// console.log(friends);
+// console.log(newLength);
 
-friends.unshift('Faysal');
-console.log(friends);
+// friends.unshift('Faysal');
+// console.log(friends);
 
 //remove elements from array
-const popped=friends.pop();
-console.log(friends);  //remove last element
-console.log(popped);
+// const popped=friends.pop();
+// console.log(friends);  //remove last element
+// console.log(popped);
 
-friends.shift(); //remove first
-console.log(friends);
+// friends.shift(); //remove first
+// console.log(friends);
 
 
-const indexElement=friends.indexOf('Safiq');
-console.log(indexElement);
-const indexElement2=friends.indexOf('Safiqa'); //not an element
-console.log(indexElement2);
+// const indexElement=friends.indexOf('Safiq');
+// console.log(indexElement);
+// const indexElement2=friends.indexOf('Safiqa'); //not an element
+// console.log(indexElement2);
 
-console.log(friends.includes('Sagor'));
-console.log(friends.includes('Bob'));
+// console.log(friends.includes('Sagor'));
+// console.log(friends.includes('Bob'));
 
-friends.push(23);
-console.log(friends);
-console.log(friends.includes('23')); //strictly checkibg type
+// friends.push(23);
+// console.log(friends);
+// console.log(friends.includes('23')); //strictly checkibg type
 
-if(friends.includes('Sagor')){
-    console.log('You have a friend named Sagor');
-}else console.log('Sagor is not in your friend list.');
+// if(friends.includes('Sagor')){
+//     console.log('You have a friend named Sagor');
+// }else console.log('Sagor is not in your friend list.');
 
 
 
@@ -153,18 +153,54 @@ if(friends.includes('Sagor')){
 // console.log(`The bill was ${bill}, the tip was ${tip} and the total payment ${bill+tip}`);
 
 
-let bill = [125,555,44];
-function calcTip(bill){
-    if(bill>=50 && bill<=300){
-        const tip=(bill*15)/100;
-        const totalBill=bill+tip;
-        return totalBill;
-    }else{
-        const tip=(bill*20)/100;
-        const totalBill=bill+tip;
-        return totalBill;
-    }
-}
+// let bill = [125,555,44];
+// function calcTip(bill){
+//     if(bill>=50 && bill<=300){
+//         const tip=(bill*15)/100;
+//         const totalBill=bill+tip;
+//         return totalBill;
+//     }else{
+//         const tip=(bill*20)/100;
+//         const totalBill=bill+tip;
+//         return totalBill;
+//     }
+// }
 
-const billCalc=[calcTip(bill[0]), calcTip(bill[1]), calcTip(bill[2])];
-console.log(`Total bills are ${billCalc}`);
+// const billCalc=[calcTip(bill[0]), calcTip(bill[1]), calcTip(bill[2])];
+// console.log(`Total bills are ${billCalc}`);
+
+
+
+// javascriptObject 
+
+ const john={
+    firstName:'John',
+    lastName:'Snow',
+    age:2010-1988,
+    job:'Teacher',
+    friends:['Sansa', 'Aria', 'Hodor', 'Dany']
+ }
+
+ console.log(john);
+ console.log(john.firstName);
+console.log(john['lastName']);
+
+const nameKey='Name';
+
+console.log(john['first'+'Name']);
+console.log(john['last'+'Name']); 
+
+// const interestedIn=prompt('What do you want to know about Jonas? Choose among firstName, lastName, age, job, friends.');
+
+// if(john[interestedIn]){
+//    console.log(john[interestedIn]);
+// }else{
+//    console.log('Wrong request. Choose among firstName, lastName, age, job, friends.');
+// }
+
+john.location='North';
+john.email='john@gmail.com';
+console.log(john);
+
+
+console.log(`${john.firstName} has ${john.friends.length} friends, and his best friend is ${john.friends[0]}`);
